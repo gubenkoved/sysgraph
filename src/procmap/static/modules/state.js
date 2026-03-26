cda37ba3eb4f5916a6ac7f6b07ee6b7f8242c45c
Eugene Gubenkov <gubenkoved@gmail.com>
2026-03-26 21:15:35 +0100

    added todo for immutable source graph + modification layer

--- src/procmap/static/modules/state.js
+++ src/procmap/static/modules/state.js
@@ -1,5 +1,11 @@
 import { Graph } from './graph.js';
 
+// TODO: introduce IMMUTABLE source graph that is only modified via extra stored
+// filters; this way it would be easy to reconstuct the filtered graph state and
+// have arbitrary undo/redo logic that would not require copying source graph;
+// also it would allow better layering of filters for instance ajdacency filter
+// should go AFTER we already filtered the grap nodes/edges by type and adjacency
+// should be calculate on ALREADY filtered graph
 export const state = {
     graph: initializeEmptyGraph(),
     highlight: null,
