4f31965e2d57b83f39268cfa2f71e5c4a3c75d4c
Eugene Gubenkov <gubenkoved@gmail.com>
2026-04-10 08:37:22 +0000

    changed color schema for domain sockets to make them easily distinguishable from processes

--- src/sysgraph-ui/modules/settings.js
+++ src/sysgraph-ui/modules/settings.js
@@ -57,14 +57,14 @@ export const highlightAlphaMultipliers = [1.0, 1.0, 0.5, 0.1]
 const defaultNodeColors = {
     process: { r: 21, g: 127, b: 200, a: 1.0 },
     socket: { r: 220, g: 75, b: 47, a: 1.0 },
-    uds: { r: 31, g: 120, b: 180, a: 1.0 },
+    uds: {r: 54, g: 188, b: 123, a: 1.0},
     pipe: { r: 169, g: 57, b: 249, a: 1.0 },
     external_ip: { r: 255, g: 103, b: 0, a: 1.0 },
 }
 
 const defaultEdgeColors = {
-    uds: { r: 31, g: 120, b: 180, a: defaultLinkOpacity },
-    uds_connection: { r: 31, g: 120, b: 180, a: defaultLinkOpacity },
+    uds: {r: 27, g: 124, b: 77, a: defaultLinkOpacity},
+    uds_connection: {r: 27, g: 124, b: 77, a: defaultLinkOpacity},
     pipe: { r: 207, g: 110, b: 255, a: defaultLinkOpacity },
     socket_connection: { r: 255, g: 76, b: 40, a: defaultLinkOpacity },
     socket: { r: 255, g: 76, b: 40, a: defaultLinkOpacity },
